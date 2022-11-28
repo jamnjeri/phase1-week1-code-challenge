16c0c45373b451068cbed832cd59b91bdae2f597
Jamila <jamnjeri@gmail.com>
2022-11-28 21:16:26 +0300

    Added comments to code challenge 1 and 2

---
+++ challenge1.js
@@ -0,0 +1,45 @@
+/*Phase 1, Week One Code Challenge*/
+
+//Challenge 1: Student Grade Generator 
+
+//Prompt user to input student marks between 0-100 and store in studentMarks variable
+let studentMarks;
+studentMarks = parseInt(prompt ("Kindly enter student marks between 0-100: "))
+console.log(`The student has ${studentMarks}%`);
+
+
+//Check if value entered is less than 0 and more than 100
+if (studentMarks > 100 || studentMarks < 0 ) {
+    console.log('Value entered is outside the desirable range')
+}
+//Check if value entered is within 0-100 (Desired Range)
+else if (studentMarks > 0 && studentMarks <=100) {
+  //console.log(studentMarks + ' is within range')
+
+  //Allocate grades
+                    //Grading System
+  //Grade A: 80-100
+  if (studentMarks > 79 && studentMarks <= 100 ){
+    console.log(`Grade: A`)
+  }
+    //Grade B: 60-79
+  else if (studentMarks >= 60 && studentMarks <= 79){
+    console.log(`Grade: B`)
+  }
+    //Grade C: 50-59
+  else if (studentMarks > 49 && studentMarks <= 59){
+    console.log(`Grade: C`)
+  }
+    //Grade D: 40-49
+  else if (studentMarks >= 40 && studentMarks <= 49){
+    console.log(`Grade: D`)
+  }
+    //Grade E: 0-40
+  else if (studentMarks < 40){
+    console.log(`Grade: E`)
+  }
+}
+//Check if value entered is not a number
+else {
+  console.log(`Value entered is not a number`)
+}
---
+++ challenge2.js
@@ -0,0 +1,33 @@
+//Challenge 2:  Speed Detector 
+//Write a program to monitor speed of vehicle
+//Speed < 70km/h --> "okay"
+//Speed > 70km/h --> demerits: 1 demerit for every 5km/h
+//More than 12 demerits --> "License suspended"
+
+//Define the function:
+function speedDetector(speedOfVehicle){
+    let demerits;
+    //Speed is a scalar quantity hence only has magnitude not direction
+    speedOfVehicle = Math.abs(speedOfVehicle)
+    console.log(`Speed = ${speedOfVehicle} km/h`)
+
+    //Any speed of 70km/h and below is considered acceptable
+    if (speedOfVehicle <= 70){
+      console.log("Ok")
+    }
+    //Any speed above 70km/h is considered too high and hence eligible to start earning demerits
+    else if (speedOfVehicle > 70){
+        //console.log("Speed above 70")
+        //Round down the demerit values to ensure only integers are displayed
+        demerits = Math.floor((speedOfVehicle -70)/5)
+        console.log(`Demerit Points: ${demerits}`)
+        //Check if the demerits are more than 12 and display said Demerits
+        if (demerits > 12){
+            console.log("License Suspended")
+        }
+        else{
+            console.log(`Treading dangerously`)
+        }
+    }
+}
+speedDetector(100)
